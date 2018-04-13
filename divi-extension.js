8b26395eaf00a9d10b70985d54d391e6f50a29d1
David Sword <davidsword@Betsy.local>
2018-04-13 15:25:23 -0700

    🤓 added vertically centering w js

--- divi-extension.js
+++ divi-extension.js
@@ -11,4 +11,23 @@ jQuery(window).ready(function() {
         jQuery('article .entry h1').html("ERROR 404 - Page Not Found");
     }
 
+    // middle-me
+    resize_banner();
+    function resize_banner() {
+		jQuery('.divi-middle-me').each(function(){
+			heightofself = jQuery(this).height()
+			heightofparent = jQuery(this).parent().height()
+			dif = Math.floor(((heightofparent) / 2) - (heightofself/2) )
+			jQuery(this).css('margin-top',dif+'px')
+		});
+    }
+    jQuery(window).bind('resizeEnd', function() { resize_banner(); });
+    jQuery(window).resize(function() {
+        if(this.resizeTO) clearTimeout(this.resizeTO);
+        this.resizeTO = setTimeout(function() {
+            jQuery(this).trigger('resizeEnd');
+        }, 500);
+    });
+
+
 });
